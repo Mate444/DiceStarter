fbb38b3f959d13aac6f5cfe04128fcce79eaa112
Mateo Hernandez <73136132+Mate444@users.noreply.github.com>
2021-06-02 16:57:33 -0300

    removes middleware

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-const isAdmin = require('../middleware/auth');
 
 const { User, Role } = require('../db');
 
-router.get('/', isAdmin, (req, res, next) => {
+router.get('/', (req, res, next) => {
   User.findAll({ include: Role })
     .then((response) => {
       res.json(response);
